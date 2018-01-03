40ec58e3d7e0bb68e69980312ded718d8f3d6313
Your Name <tarunkhandelwal@live.in>
2018-01-03 13:06:04 +0530

    Applied Routing and new page opening

--- app/scripts/datafetch.js
+++ app/scripts/datafetch.js
@@ -1,7 +1,8 @@
 
 var lowerFoldContainer = $('#lowerFold');
+
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', '../data.json');
+ourRequest.open('GET', 'http://hellobanswara.com/n.php');
 
 ourRequest.onload = function(){
   var ourNews = JSON.parse(ourRequest.responseText);
@@ -18,7 +19,7 @@ function renderNewsCards(data){
 
   for(i = 0; i < 4; i++){
     newsCards += '<div class="col-md-6 col-lg-3">\n' +
-      '                  <div class="news-card">\n' +
+      '                  <div class="news-card _default">\n' +
       '\n' +
       '                    <div class="_news-img">\n' +
       '                      <a href="#">\n' +
@@ -28,12 +29,13 @@ function renderNewsCards(data){
       '\n' +
       '                    <div class="_news-details-box">\n' +
       '                      <div class="_news-title"><a href="#">' + data[i].judul + '</a></div>\n' +
+      '                      <div class="_news-details">शहरके लीयो कॉलेज में मंगलवार को छात्राओं ने महिलाओं की सुरक्षा में मददगार रक्षा सूत्र एप डाउनलोड किया।</div>\n' +
       '                    </div>\n' +
       '\n' +
       '                  </div>\n' +
       '                </div>'
   }
 
-  var newsContainer = '<div class="news-card-stack row  mt-sm-3">' + newsCards + '</div>';
+  var newsContainer = '<h3 class="section-title f-hindi mb-4">खबर जो आपके काम आये </h3><div class="news-card-stack row  mt-sm-3">' + newsCards + '</div>';
   lowerFoldContainer.append(newsContainer);
 };
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -35,3 +35,54 @@ $('.closeSlide').click(
   }
 );
 
+
+
+
+/*
+ *  Routing News
+ */
+
+$(function() {
+  String.prototype.decodeHTML = function() {
+    return $('<div>', {html: '' + this}).html();
+  };
+
+  var $main = $('main'),
+
+    init = function() {
+
+    },
+
+    ajaxLoad = function(html) {
+      document.title = html
+        .match(/<title>(.*?)<\/title>/)[1]
+        .trim()
+        .decodeHTML();
+
+      init();
+    },
+
+    loadPage = function(href) {
+      $main.load(href + ' main>*', ajaxLoad);
+    };
+
+  init();
+
+  $(window).on('popstate', function(e) {
+    // if (e.originalEvent.state !== null) {
+      loadPage(location.href);
+    // }
+  });
+
+  $(document).on('click', 'a, area', function() {
+    var href = $(this).attr('href');
+
+    if (href.indexOf(document.domain) > -1
+      || href.indexOf(':') === -1)
+    {
+      history.pushState({}, '', href);
+      loadPage(href);
+      return false;
+    }
+  });
+});
